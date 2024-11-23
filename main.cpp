8fa57375fc2bf55204ae871822f07d29448ad9c5
shakil-innovate <shakil.innovate@gmail.com.>
2024-11-23 11:46:53 +0600

    add game over image

--- main.cpp
+++ main.cpp
@@ -58,6 +58,11 @@ int main(int argc, char* argv[]) {
 
             renderGame(renderer, snake, food, font, score);
         }
+
+        if (quit) {
+            showImage(renderer, "image/gameover.png",3000); // Show the game over image for 3 seconds
+            break; // Exit the game loop
+        }
     }
 
     cleanupSDL(window, renderer, font);
@@ -138,7 +143,12 @@ void cleanupSDL(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
 
 // Display intro image
 void renderIntro(SDL_Renderer* renderer) {
-    showImage(renderer, "image/cover.png", 5000);
+    showImage(renderer, "image/cover.png", 3000);
+}
+
+bool isPointInsideButton(int mouseX, int mouseY, SDL_Rect& buttonRect) {
+    return mouseX >= buttonRect.x && mouseX <= buttonRect.x + buttonRect.w &&
+           mouseY >= buttonRect.y && mouseY <= buttonRect.y + buttonRect.h;
 }
 
 // Generate random food position
